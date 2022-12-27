bc6f2c8a6833df34dcce0d6136d0c45504abb798
tigerlab <tiggers12@hotmail.com>
2022-12-27 16:25:31 +0700

    ss

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int N,int M){
+if(N>0){
+	for(int i = 0; i < N ; i++){
+		if(M > 0){
+			for(int j = 0; j < M ; j++){
+				cout << "O";
+			}
+			cout << "\n";
+			}else{
+				cout <<"Invalid input" ;
+				break; 
+			}
+		}
+	}
+	else{
+		cout << "Invalid input";
+	}
+}
 
 int main(){
 	
